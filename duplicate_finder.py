a20cf7f2d8a4a01bfa5bb5bcd0ff6caf273baaa0
Chris <mumfordnsons@gmail.com>
2016-02-15 20:34:09 +0000

    Add time console output
    This allows us to see how long the program execution takes.

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -1,4 +1,6 @@
-import sys
+import sys, time
 import modules.file_processor as file_processor
 
+print time.strftime("%Y-%m-%d %H:%M:%S")
 file_processor.process(sys.argv[1])
+print time.strftime("%Y-%m-%d %H:%M:%S")
