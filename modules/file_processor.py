d69b81683144c043956b8bedc3f23ca3350f1229
bombadillo <mumfordnsons@gmail.com>
2016-02-29 12:24:54 +0000

    Add branch factMeasuresWithoutValues
    This branch will check for duplicates for fact measures excluding the values

--- modules/file_processor.py
+++ modules/file_processor.py
@@ -12,14 +12,16 @@ def process(file_name):
     d = collections.defaultdict(list)
     with open(file_name, 'r') as datafile:
         for line in datafile:
+            line_split = line.split('|')
+            new_line = line_split[0] + line_split[1] + line_split[2] + line_split[3]
             current_line_number += 1
-            id = hashlib.sha256(line).digest()
+            id = hashlib.sha256(new_line).digest()
             k = id[0:2]
             v = id[2:]
             if v in d[k]:
                 dups_found += 1
-                line = line.replace('\n', '')
-                string_to_write = '"{0}" on Line {1} in {2}{3}'.format(line, current_line_number, file_name, Config.new_line)
+                new_line = new_line.replace('\n', '')
+                string_to_write = '"{0}" on Line {1} in {2}{3}'.format(new_line, current_line_number, file_name, Config.new_line)
                 file_writer.write_string(string_to_write, Config.output_file)
             else:
                 d[k].append(v)
